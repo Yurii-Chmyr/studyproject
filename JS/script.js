66c535e57f97f760a0485c4a2f52899c3c0972ad
Yuri <tchmir2014@gmail.com>
2025-02-28 18:12:52 +0200

    3 task

--- JS/script.js
+++ JS/script.js
@@ -22,37 +22,100 @@
       
 // console.log(personalMovieDB);
 
-if (4 == 9) {
-    console.log('Ok!');
-} else {
-    console.log('Not Ok!');
-};
+// if (4 == 9) {
+//     console.log('Ok!');
+// } else {
+//     console.log('Not Ok!');
+// };
 
 
 
-if (num < 49) {
-    console.log('Error');
-} else if (num > 100) {
-    console.log('Too much');
-} else {
-    console.log('Ok');
-}
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Too much');
+// } else {
+//     console.log('Ok');
+// }
 
-(num === 59) ? console.log('ok') : console.log('Error');
+// (num === 59) ? console.log('ok') : console.log('Error');
 
-const num = 50;
+// const num = 50;
 
-switch (num) {
-    case 49:
-        console.log('Not True');
-        break;
-    case 100:
-        console.log('Not True2');
-        break;
-    case 50:
-        console.log('True');
-        break;
-    default:
-        console.log('LoL');
-        break;
-}
+// switch (num) {
+//     case 49:
+//         console.log('Not True');
+//         break;
+//     case 100:
+//         console.log('Not True2');
+//         break;
+//     case 50:
+//         console.log('True');
+//         break;
+//     default:
+//         console.log('LoL');
+//         break;
+// }
+
+// const hamburger = 5;
+// const fries = 0;
+
+// if (hamburger && fries) {
+//     console.log('Я ситий');
+// }
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 0;
+
+// if (hamburger === 3 && cola === 1 && fries) {
+//     console.log('Всі ситі');
+// } else {
+//     console.log('Ми йдемо');
+// }
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+//     console.log('Всі довольні');
+// } else {
+//     console.log('Ми йдемо');
+// }
+
+// console.log(hamburger === 3 && (cola === 2 || fries === 3) && nuggets);
+
+// let johnReport, alexReport, samReport, mariaReport = 'done';
+
+// console.log(johnReport || alexReport || samReport || mariaReport)
+
+// console.log(!0);
+
+// console.log(NaN||2||undefined);
+
+// console.log (NaN && 2 && undefined);
+
+// console.log(1 && 2 && 3);
+
+// console.log(!1 && 2 || !3);
+
+// console.log (25 || null && !3);
+
+// console.log (NaN || null || !3 || undefined || 5);
+
+// console.log (NaN || null && !3 && undefined || 5);
+
+// console.log (5 === 5 && 3 > 1 || 5);
+
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+// }
