6f816bc97c7a1a0e7e0e1532c652de0527b8274a
Yuri <tchmir2014@gmail.com>
2025-02-28 17:06:34 +0200

    2 task

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,58 @@
 "use strict"
 
 
-const numberOfFilms = +prompt('Скільки фільмів ви вже подивились?', '');
+// const numberOfFilms = +prompt('Скільки фільмів ви вже подивились?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat:false
+// const personalMovieDB = {
+//    count: numberOfFilms,
+//    movies: {},
+//     actors: {},
+//     genres: [],
+//    privat:false
 
+// };
+
+// const a = prompt('Один із останніх переглянутих фільмів?', ''),
+//      b = prompt('На скільки ви його оціните?', ''),
+//      c = prompt('Один із останніх переглянутих фільмів?', ''),
+//      d = prompt('На скільки ви його оціните?', '');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+      
+// console.log(personalMovieDB);
+
+if (4 == 9) {
+    console.log('Ok!');
+} else {
+    console.log('Not Ok!');
 };
 
-const a = prompt('Один із останніх переглянутих фільмів?', ''),
-      b = prompt('На скільки ви його оціните?', ''),
-      c = prompt('Один із останніх переглянутих фільмів?', ''),
-      d = prompt('На скільки ви його оціните?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-      
-console.log(personalMovieDB);
+
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Too much');
+} else {
+    console.log('Ok');
+}
+
+(num === 59) ? console.log('ok') : console.log('Error');
+
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('Not True');
+        break;
+    case 100:
+        console.log('Not True2');
+        break;
+    case 50:
+        console.log('True');
+        break;
+    default:
+        console.log('LoL');
+        break;
+}
