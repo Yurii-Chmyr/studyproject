61f482bf36e9ec8126b8d0e767d19df823fbc16b
Yuri <tchmir2014@gmail.com>
2025-03-05 21:19:34 +0200

    yrok plus dz

--- JS/script.js
+++ JS/script.js
@@ -780,3 +780,143 @@
 //     return a - b;
 // }
 
+// let a = 5,
+//     b = a;
+
+//     b = b + 5;
+
+    // console.log(b);
+    // console.log(a);
+
+    // const obj = {
+    //     a: 5,
+    //     b: 1
+    // };
+
+    // const copy = obj;
+
+    // copy.a = 10;
+
+    // console.log(copy);
+    // console.log(obj);
+
+
+    // function copy(mainObj) {
+    //     let objCopy = {};
+
+    //     let key;
+    //     for (key in mainObj) {
+    //         objCopy[key] = mainObj[key];
+    //     }
+
+    //     return objCopy;
+    // }
+
+    // const numbers = {
+    //     a: 2,
+    //     b: 5,
+    //     c: {
+    //         x: 7,
+    //         y: 4
+    //     }
+    // };
+
+    // const newNumbers = copy(numbers);
+
+    // newNumbers.a = 10;
+    // newNumbers.c.x = 10;
+
+    // console.log(newNumbers);
+    // console.log(numbers);
+
+    // const add = {
+    //     d: 17,
+    //     e: 20
+    // };
+
+    // console.log(Object.assign(numbers, add))
+
+    // const clone = Object.assign({}, add);
+
+    // clone.d = 20;
+
+    // console.log(add);
+    // console.log(clone);
+
+//     const oldArray = ['a', 'b', 'c'];
+//     const newArray = oldArray.slice();
+
+//     newArray[1] = 'sdsdsd';
+//     console.log(newArray);
+//     console.log(oldArray);
+
+//     const video = ['youtube', 'vimeo', 'rutube'],
+//           blogs = ['wordpress', 'livejournal', 'blogger'],
+//           internet = [...video, ...blogs, 'vk', 'facebook'];
+
+//           console.log(internet);
+
+// function log (a, b ,c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+
+// }
+
+// const num = [2, 5, 7];
+
+// log(...num);
+
+// const array = ["a", "b"];
+
+// const newAaray = [...array];
+
+// const q = {
+//     one: 1,
+//     two: 2
+// };
+
+// const newObj = {...q};
+
+// const personalPlanPeter = {
+//     name: "Peter",
+//     age: "29",
+//     skills: {
+//         languages: ['ru', 'eng'],
+//         programmingLangs: {
+//             js: '20%',
+//             php: '10%'
+//         },
+//         exp: '1 month'
+//     },
+
+//     showAgeAndLangs: function(plan) {
+//         const {age} = plan;
+//         const {languages} = plan.skills;
+//         let str = `Мне ${age} и я владею языками: `;
+
+//         languages.forEach(function(lang) {
+//             str += `${lang.toUpperCase()} `;
+//         });
+
+//         return str;
+//     }
+
+// };
+
+// personalPlanPeter.showAgeAndLangs(personalPlanPeter);
+
+// function showExperience(plan) {
+//     console.log(plan["skills"]["exp"]);
+// }
+
+
+// function showProgrammingLangs(plan) {
+//     let str = '';
+//     const {programmingLangs} = plan.skills;
+//     for (let key in programmingLangs) {
+//         str += `Язык ${key} изучен на ${programmingLangs[key]}\n`
+//     }
+
+//     return str;
+// }
