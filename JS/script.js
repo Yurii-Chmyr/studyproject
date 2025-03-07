e85d8b52ecd32551d6eb855e09b07f3e1b1cf6e0
Yuri <tchmir2014@gmail.com>
2025-03-07 23:22:58 +0200

    another yroks

--- JS/script.js
+++ JS/script.js
@@ -920,3 +920,280 @@
 
 //     return str;
 // }
+
+// let str = "some";
+// let strObj = new String(str);
+
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+// console.dir([1, 2, 3]);
+
+// const soldier = {
+//     health: 400,
+//     armor :100,
+//     sayHello: function() {
+//         console.log('Hello');
+//     }
+// };
+
+// const john = Object.create(soldier);
+
+
+// const john = {
+//     health: 100
+// };
+
+// john.__proto__ = soldier;
+
+// Object.setPrototypeOf(john, soldier);
+
+// console.log(john.armor);
+// john.sayHello();
+
+// const numberOfFilms = +prompt('Скільки фільмів ви вже подивились?', '');
+
+// const personalMovieDB = {
+//    count: 0,
+//    movies: {},
+//     actors: {},
+//     genres: [],
+//    privat:false,
+
+//    start: function() {
+//     personalMovieDB.count = +prompt('Скільки фільмів ви вже подивились?', '');
+
+//     while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+//         personalMovieDB.count = +prompt('Скільки фільмів ви вже подивились?', '');
+
+//     }
+// },
+
+// rememberMyFilms: function () {
+// for (let i = 0; i < 2; i++) { 
+//     const a = prompt('Один із останніх переглянутих фільмів?', ''),
+//           b = prompt('На скільки ви його оціните?', '');
+
+//           if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//             personalMovieDB.movies[a] = b;
+//             console.log('done');
+//           } else {
+//             console.log('error');
+//             i--;
+//           }
+//         }
+// },
+
+// detectPersonalLevel: function() {
+//     if (personalMovieDB.count < 10) {
+//         console.log("Переглянуто мало фільмів");
+//     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//         console.log("Ви класичний глядач");
+
+//     } else if (personalMovieDB.count >= 30) {
+//         console.log("Ви кіноман!");
+//     } else {
+//         console.log("Сталась помилка!");
+//     }
+// },
+
+// showMyDb: function (hidden) {
+//     if (!hidden) {
+//         console.log(personalMovieDB)
+//     }
+        
+// },
+
+// writeYourGenres: function() {
+//     for ( let i = 1; i < 2; i++) {
+        // let genre = prompt(`Ваш улюблений жанр під номером ${i}`);
+
+        // if (genre === "" || genre == null) {
+        //     console.log('Ви ввели некоретні дані або не ввелі їх взагалі');
+        //     i--;
+        // } else {
+        //     personalMovieDB.genres[i - 1] = genre;
+
+        // let genres = prompt(`Введіть ваші улюблені жанри через кому`).toLowerCase();
+           
+        // if (genres === "" || genres == null) {
+        //     console.log('Ви ввели некоретні дані або не ввелі їх взагалі');
+        //     i--;
+        // } else {
+        //     personalMovieDB.genres = genres.split(', ');
+        //     personalMovieDB.genres.sort();
+
+        // }
+
+//        personalMovieDB.genres.forEach((item, i) => {
+//         console.log(`Улюблений жанр ${i + 1} - це ${item}`);
+//        });
+
+//     } 
+    
+// },
+
+// toggleVisibleMyDB: function () {
+//     if (personalMovieDB.privat) {
+//         personalMovieDB.privat = false;
+//     } else {
+//         personalMovieDB.privat = true;
+//     }
+// },
+
+// };
+
+
+// const shoppingMallData = {
+//     shops: [
+//         {
+//             width: 10,
+//             length: 5
+//         },
+//         {
+//             width: 15,
+//             length: 7
+//         },
+//         {
+//             width: 20,
+//             length: 5
+//         },
+//         {
+//             width: 8,
+//             length: 10
+//         }
+//     ],
+//     height: 5,
+//     moneyPer1m3: 30,
+//     budget: 50000
+// }
+
+// function isBudgetEnough(data) {
+//     let square = 0;
+//     let volume = 0;
+
+//     data.shops.forEach(shop => {
+//         square += shop.width * shop.length;
+//     });
+
+//     volume = data.height * square;
+
+//     if (data.budget - (volume * data.moneyPer1m3) >= 0) {
+//         return 'Бюджета достаточно';
+//     } else {
+//         return 'Бюджета недостаточно';
+//     }
+// }
+
+// isBudgetEnough(shoppingMallData);
+
+// const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+// function sortStudentsByGroups(arr) {
+//     arr.sort();
+//     const a = [], b = [], c = [], rest = [];
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (i < 3) {
+//             a.push(arr[i]);
+//         } else if (i < 6) {
+//             b.push(arr[i]);
+//         } else if (i < 9) {
+//             c.push(arr[i]);
+//         } else {
+//             rest.push(arr[i]);
+//         }
+//     }
+//     return [a,b,c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`]
+// }
+
+// sortStudentsByGroups(students);
+
+// to string
+
+// 1)
+
+// console.log(typeof(String(null)));
+// console.log(typeof(String(4)));
+
+// 2)
+
+// console.log(typeof(5 + ''));
+
+// const num = 5;
+
+// console.log("https://vk.com/catalog/" + num);
+
+// const fontSize = 26 + 'px';
+
+// To Number
+
+// 1)
+
+// console.log(typeof(Number('4')));
+
+
+// 2)
+
+// console.log(typeof(+'4'));
+
+// 3)
+
+// console.log(typeof(parseInt("15px", 10)));
+
+
+// let answ = +prompt("Hello", "");
+
+// to boolean
+
+// 1)
+// 0, '', null, undefined, NaN; // false
+
+// let switcher = 1;
+
+// if (switcher) {
+//     console.log('Working....');
+// }
+
+
+// 3)
+
+// console.log(typeof(!!'444444'));
+
+// let number = 5; debugger
+
+// function logNumber() {
+//     let number = 4; debugger
+//     console.log(number);
+// }
+
+// number = 6;
+
+// logNumber(); debugger
+
+// function createCounter() {
+//     let counter = 0;
+
+//     const myFunction = function() {
+//         counter = counter + 1;
+//         return counter;
+//     }
+
+//     return myFunction;
+// }
+
+// const increment = createCounter();
+// const c1 = increment();
+// const c2 = increment();
+// const c3 = increment();
+
+// console.log(c1, c2, c3);
+
+// for (let i = 0; i < 9; i++) {
+//     for (let j = 0; j < 9; j++) {
+//         let num = 3;
+//     }
+
+//     console.log(num);
+// }
+
